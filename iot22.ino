403c0ebc5110d6c5cbbf0e3aaa7ef2496476ef19
ishikaborkar12345 <ishikaborkar093@gmail.com>
2026-03-11 22:02:20 +0530

    commit2

---
+++ iot22.ino
@@ -0,0 +1,14 @@
+//This is my first IOT program
+ #define my_led 7 //global declaration, LED pin no. 7
+
+void setup() {
+  pinMode(my_led ,OUTPUT);
+
+}
+
+void loop() {
+  digitalWrite(my_led,HIGH);
+  delay(1000);
+  digitalWrite(my_led,LOW);
+  delay(1000);
+}
